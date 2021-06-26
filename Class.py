be0a89b678980124af8c4a7531363b031082c0d8
Razvan-Nanu <76228755+Razvan-Nanu@users.noreply.github.com>
2021-06-25 21:42:18 -0700

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,11 @@
+class Celebrity:
+    Name: 'Lebron James'
+    Job: 'Athlete'
+
+class Residency(Celebrity):
+    Area: 'Los Angeles'
+    Zipcode: 12345
+
+class Family(Celebrity):
+    Married: 'Yes'
+    Children: 'Yes'
